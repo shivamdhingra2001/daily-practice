6366608f87302f031e0c8231098edab91beffb18
Shivam <shivamdhingra2001@gmail.com>
2025-11-16 19:20:23 +0530

    added 1513

---
+++ Leetcode/Medium/Leetcode -1513.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSub(string s) {
+        const int MOD = 1e9 + 7;
+        long long count = 0, ans = 0;
+
+        for (char c : s) {
+            if (c == '1') {
+                count++;
+                ans = (ans + count) % MOD;
+            } else {
+                count = 0;
+            }
+        }
+
+        return ans;
+    }
+};
