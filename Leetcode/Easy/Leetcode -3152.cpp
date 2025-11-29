bdedc3753693b7c3dc269fa9a7db8f53f7cd2819
Shivam <shivamdhingra2001@gmail.com>
2025-11-29 14:49:00 +0530

    added 3152

---
+++ Leetcode/Easy/Leetcode -3152.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        long long sum = 0;
+        for (int x : nums)
+            sum += x;
+        return sum % k;
+    }
+};
